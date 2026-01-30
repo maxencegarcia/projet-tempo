3de861c26ea4f79e252d6b556098a75ddffe1362
mgarcia6 <saeinfob1@gmail.com>
2026-01-30 21:05:58 +0100

    Update connexion_db.py

--- connexion_db.py
+++ connexion_db.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
-
+from flask import g
 import pymysql.cursors
 import os
 
@@ -13,31 +12,7 @@ def get_db():
             port=int(os.environ.get("MYSQLPORT", 3306)),
             charset="utf8mb4",
             cursorclass=pymysql.cursors.DictCursor,
-            autocommit=True
+            autocommit=True,
+            connect_timeout=10
         )
     return g.db
-
-def activate_db_options(db):
-    cursor = db.cursor()
-    # Vérifier et activer l'option ONLY_FULL_GROUP_BY si nécessaire
-    cursor.execute("SHOW VARIABLES LIKE 'sql_mode'")
-    result = cursor.fetchone()
-    if result:
-        modes = result['Value'].split(',')
-        if 'ONLY_FULL_GROUP_BY' not in modes:
-            print('MYSQL : il manque le mode ONLY_FULL_GROUP_BY')   # mettre en commentaire
-            cursor.execute("SET sql_mode=(SELECT CONCAT(@@sql_mode, ',ONLY_FULL_GROUP_BY'))")
-            db.commit()
-        else:
-            print('MYSQL : mode ONLY_FULL_GROUP_BY  ok')   # mettre en commentaire
-    # Vérifier et activer l'option lower_case_table_names si nécessaire
-    cursor.execute("SHOW VARIABLES LIKE 'lower_case_table_names'")
-    result = cursor.fetchone()
-    if result:
-        if result['Value'] != '0':
-            print('MYSQL : valeur de la variable globale lower_case_table_names differente de 0')   # mettre en commentaire
-            cursor.execute("SET GLOBAL lower_case_table_names = 0")
-            db.commit()
-        else :
-            print('MYSQL : variable globale lower_case_table_names=0  ok')    # mettre en commentaire
-    cursor.close()
